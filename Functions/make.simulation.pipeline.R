afb6edf4df00cf4cf3b111d67e2e639610a4086b
TGuillerme <guillert@tcd.ie>
2021-05-13 17:24:16 +0100

    INIT make.simulation.pipeline

---
+++ Functions/make.simulation.pipeline.R
@@ -0,0 +1,27 @@
+#' @title Make the simulation pipeline
+#'
+#' @description Set the variables for running one simulation
+#'
+#' @param sim.data A named list of parameters for the data simulation (default is \code{list{n.traits = 2, speciation = 1, n.taxa = 200}})
+#' @param verbose whether to be verbose (\code{TRUE}, default) or not (\code{FALSE})
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+make.simulation.pipeline <- function(sim.data = list(n.traits   = 2,
+                                                     speciation = 1,
+                                                     n.taxa     = 200),
+                                     verbose  = TRUE) {
+
+
+
+n_traits <- 2
+speciation_rate <- 1
+n_taxa <- 200
+
+    return()
+}
