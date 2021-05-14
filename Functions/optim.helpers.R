b672b4a3efadbb6ee0c842918cf973792ae09bfe
TGuillerme <guillert@tcd.ie>
2021-05-14 14:38:32 +0100

    INIT optim.helpers

---
+++ Functions/optim.helpers.R
@@ -0,0 +1,35 @@
+#' @title The diagnosis function for optim.replicate
+#'
+#' @description 
+#'
+#' @param 
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+optim.diagnose <- function() {
+
+    return()
+}
+
+#' @title The summary function for optim.replicate
+#'
+#' @description 
+#'
+#' @param 
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+#' 
+optim.summarise <- function() {
+
+    return()
+}
