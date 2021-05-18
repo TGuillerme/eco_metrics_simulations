99a11d035f8fca2598d9fdf39cf9605721bc12ad
TGuillerme <guillert@tcd.ie>
2021-05-18 12:55:47 +0100

    summary and plot prototypes

---
+++ Functions/plot.results.R
@@ -0,0 +1,19 @@
+#' @title Plot results
+#'
+#' @description Plotting the results
+#'
+#' @param results the results matrix (typically from summarise.results)
+#' @param what optional, a sub-selection of the results to plot
+#' @param ... any arguments to be passed to \code{lines}.
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+plot.results <- function(results, what, ...) {
+
+    return()
+}
---
+++ Functions/summarise.results.R
@@ -0,0 +1,20 @@
+#' @title Summarise the results
+#'
+#' @description Summarises the results (from the replications)
+#'
+#' @param results the results or the path to the results
+#' @param what optional, which specific results to summarise
+#' @param scale logical, whether to scale the results relative to the the random reduction (TRUE, default) or not (FALSE).
+#' @param time optional, whether to summarise the running time (TRUE) or the results (FALSE, default)
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+summarise.results <- function(results, what, scale = TRUE, time = FALSE) {
+
+    return()
+}
