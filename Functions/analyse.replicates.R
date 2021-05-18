2cc8cf63d7c3f9a1e6f793d217499adfc312d840
TGuillerme <guillert@tcd.ie>
2021-05-18 12:51:13 +0100

    Fixed maximum

---
+++ Functions/analyse.replicates.R
@@ -0,0 +1,19 @@
+#' @title Analyse replicates
+#' 
+#' @description Analyse the replicates results
+#'
+#' @param results the results list or a path to the file
+#' @param what optional, a sub-selection of the results to plot
+#' @param ... any arguments to be passed to \code{lines}.
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+analyse.replicates <- function(results) {
+
+    return()
+}
