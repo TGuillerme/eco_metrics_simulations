1c6b32b60aed56d589390147fb119b76e1877ba5
TGuillerme <guillert@tcd.ie>
2021-07-23 15:10:57 +0100

    moved extract.table as its own function

---
+++ Functions/extract.table.R
@@ -0,0 +1,97 @@
+#' @title extract.table
+#'
+#' @description extract the scaled results per metric
+#'
+#' @param results the list of results (per stressor)
+#' @param centre whether to centre the data on the null results (TRUE; default)
+#' @param scale.method optional, which scaling method to use. Either "within" for scaling each metric within each stressor (abs(max(metric$stressor))), "between" for scaling each metric between each stressor (abs(max(unlist(metric)))) or left empty for no scaling.
+#' 
+#' @examples
+#'
+#' @seealso
+#' 
+#' @author Thomas Guillerme
+#' @export
+
+## Extract table
+extract.table <- function(results, centre = TRUE, scale.method) {
+
+
+    # stop("DEBUG extract.table")
+    # results <- all_results
+    
+    ## Centre all the results
+    results <- lapply(results, centre.null, centre = centre)
+
+    ## Scale all the metrics between each stressor
+    if(!missing(scale.method)) {
+        if(scale.method == "between") {
+            ## Merging all the results together to the the max per metric
+            metric_max <- apply(do.call(rbind, results), 2, function(x) return(max(abs(x))))
+            #TG: use absolute values?
+            ## Scaling each result by its maximum
+            results <- lapply(results, function(X, max) t(apply(X, 1, function(x, max) return(x/max), max = metric_max)), max = metric_max)
+        }
+        ## Scale all the metrics within each stressor
+        if(scale.method == "within") {
+            ## Merging all the results together to the the max per metric
+             results <- lapply(results, function(one_results) apply(one_results, 2, function(X) return(X/max(abs(X)))))
+        }
+    }
+
+    ## Sort the results per metric
+    results <- lapply(results, sort.results.names, centre = centre)
+
+    return(results)
+}
+
+## Centring on the null
+centre.null <- function(one_results, centre = TRUE) {
+
+    ## Finding the null results
+    null_results <- grep("random", colnames(one_results$results_table))
+
+    ## Get the pairs of columns
+    metrics_names <- gsub("stressor_", "", gsub("random_", "", colnames(one_results$results_table)))
+    pairs <- lapply(as.list(unique(metrics_names)), function(x, names) which(names %in% x), names = metrics_names)
+    
+    ## Centre each pair on the null
+    if(centre) {
+        centre_results <- lapply(pairs, function(pair, matrix) return(matrix[, pair[2]] - matrix[, pair[1]]), matrix = one_results$results_table)
+        output <- do.call(cbind, centre_results)
+    } else {
+        output <- one_results$results_table
+    }
+
+    if(centre) {
+        colnames(output) <- unique(metrics_names)
+    } 
+    return(output)
+}
+
+## Sort the results per name
+sort.results.names <- function(results, centre) {
+
+    ## Find the different removal levels
+    levels <- unique(gsub(".*_rm", "", colnames(results)))
+     
+    if(centre) {   
+        ## Find the metrics names    
+        metrics <- unique(gsub("_rm.*", "", colnames(results)))
+    } else {
+        ## Find the metrics names    
+        metrics <- unique(gsub("stressor_", "", gsub("random_", "", gsub("_rm.*", "", colnames(results)))))     
+    }
+
+    ## Sort the metrics names
+    sorted_metrics <- unlist(lapply(as.list(metrics), function(x, levels) paste(x, levels, sep = "_rm"), levels = levels))
+
+    if(!centre) {
+        ## Sort the metric names
+        sorted_metrics <- c(apply(cbind(paste0("random_", sorted_metrics), paste0("stressor_", sorted_metrics)), 1, c))
+    }
+
+    ## Return the sorted column names
+    return(results[, match(sorted_metrics, colnames(results))])
+}
+
--- Functions/summarise.results.R
+++ Functions/summarise.results.R
@@ -3,7 +3,8 @@
 #' @description Summarises the results (from the replications)
 #'
 #' @param results the results or the path to the results
-#' @param scale logical, whether to scale the results relative to the the random reduction (TRUE, default) or not (FALSE).
+#' @param scale.method optional, which scaling method to use. Either "within" for scaling each metric within each stressor (abs(max(metric$stressor))), "between" for scaling each metric between each stressor (abs(max(unlist(metric)))) or left empty for no scaling.
+#' @param centre whether to centre the data on the null results (TRUE; default)
 #' @param cent.tend function, the central tendency function (default is median)
 #' @param prob the probability quantiles to display (default is c(0.975, 0.75, 0.25, 0.025))
 #' @param output.by whether to return the output sorted by stressor ("stressor"; default) or by metric ("metric").
@@ -15,7 +16,7 @@
 #' @author Thomas Guillerme
 #' @export
 
-summarise.results <- function(results, scale = TRUE, centre = TRUE, cent.tend = median, prob = c(0.975, 0.75, 0.25, 0.025), output.by = "stressor") {
+summarise.results <- function(results, scale.method = "between", centre = TRUE, cent.tend = median, prob = c(0.975, 0.75, 0.25, 0.025), output.by = "stressor") {
 
     ## Make the results into a list (or not)
     if(all(names(results) %in% c("results_table", "diagnosis", "output_save", "n_iterations"))) {
@@ -23,82 +24,12 @@ summarise.results <- function(results, scale = TRUE, centre = TRUE, cent.tend = 
     }
 
     ## Extracting the results table
-    results_table <- extract.table(results, scale, centre)
+    results_table <- extract.table(results, centre, scale.method)
 
     ## Summarise the results
     return(lapply(results_table, summarise.table, cent.tend, prob))
 }
 
-
-## Centring on the null
-centre.null.scale <- function(results, scale = FALSE, centre) {
-
-    ## Finding the null results
-    null_results <- grep("random", colnames(results$results_table))
-
-    ## Get the pairs of columns
-    metrics_names <- gsub("stressor_", "", gsub("random_", "", colnames(results$results_table)))
-    pairs <- lapply(as.list(unique(metrics_names)), function(x, names) which(names %in% x), names = metrics_names)
-    
-    ## Centre each pair on the null
-    if(centre) {
-        centre_results <- lapply(pairs, function(pair, matrix) return(matrix[, pair[2]] - matrix[, pair[1]]), matrix = results$results_table)
-        output <- do.call(cbind, centre_results)
-    } else {
-        output <- results$results_table
-    }
-
-    ## Scale each centred results
-    if(scale) {
-        if(centre) {
-            output <- apply(output, 2, function(X) return(X/max(abs(X))))
-        } else {
-            stop("DEBUG: TODO: allow scaling while not centring")
-            #TG: use something with pairs
-        }
-    }
-    if(centre) {
-        colnames(output) <- unique(metrics_names)
-    } 
-    return(output)
-}
-
-## Sort the results per name
-sort.results.names <- function(results, centre) {
-
-    ## Find the different removal levels
-    levels <- unique(gsub(".*_rm", "", colnames(results)))
-     
-    if(centre) {   
-        ## Find the metrics names    
-        metrics <- unique(gsub("_rm.*", "", colnames(results)))
-    } else {
-        ## Find the metrics names    
-        metrics <- unique(gsub("stressor_", "", gsub("random_", "", gsub("_rm.*", "", colnames(results)))))     
-    }
-
-    ## Sort the metrics names
-    sorted_metrics <- unlist(lapply(as.list(metrics), function(x, levels) paste(x, levels, sep = "_rm"), levels = levels))
-
-    if(!centre) {
-        ## Sort the metric names
-        sorted_metrics <- c(apply(cbind(paste0("random_", sorted_metrics), paste0("stressor_", sorted_metrics)), 1, c))
-    }
-
-    ## Return the sorted column names
-    return(results[, match(sorted_metrics, colnames(results))])
-}
-
-## Extract table
-extract.table <- function(results, scale, centre) {
-    ## Centre and scale the results
-    results <- lapply(results, centre.null.scale, scale = scale, centre = centre)
-
-    ## Sort the results per metric
-    results <- lapply(results, sort.results.names, centre = centre)
-
-    return(results)
-}
 
 ## Summarise the results
 summarise.table <- function(results, cent.tend, prob) {
